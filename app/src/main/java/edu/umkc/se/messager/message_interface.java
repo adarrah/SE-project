4ea586e8465769988c5b3f4cb42fd0d5ee5c0483
Aaron Darrah <add522@mail.umkc.edu>
2018-09-23 21:28:57 -0500

    Version .01
    Added sendText function and unit test for it. Set up for next iteration implementation of sending texts to other phones.

--- app/src/main/java/edu/umkc/se/messager/message_interface.java
+++ app/src/main/java/edu/umkc/se/messager/message_interface.java
@@ -1,5 +1,6 @@
 package edu.umkc.se.messager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +12,8 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import org.w3c.dom.Text;
 
@@ -20,12 +23,13 @@ public class message_interface extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_interface);
-        final EditText outgoing_message;
-        TextView sent_text, incoming_message;
+        final Context ctx = this;
+        EditText outgoing_message;
+        final TextView sent_text, incoming_message;
         Button send_message;
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,20 +37,27 @@ public class message_interface extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
 
+        sent_text = (TextView) findViewById(R.id.sent_text);
         send_message =(Button) findViewById(R.id.send_button);
         outgoing_message = (EditText) findViewById(R.id.outgoing_text);
+        final String outgoing_message_string = outgoing_message.getText().toString();
         send_message.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                sendText(outgoing_message.getText().toString());
+                if(sendText(outgoing_message_string)){
+                    sent_text.setText(outgoing_message_string);
+                }
+                else{
+                    Toast.makeText(ctx, "Message Failed to send",Toast.LENGTH_SHORT);
+                }
+
             }
         });
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_message_interface, menu);
@@ -66,10 +77,13 @@ public class message_interface extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
-    private void sendText(String message){
-        
+    public static boolean sendText(String message){
+        if(message.isEmpty())
+            return false;
+        else
+            return true;
     }
 
 }
